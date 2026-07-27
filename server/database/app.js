12e21279524a84b70abf71fe785854dcc3f9665a
Udhhjeiek <bansodeharsh22@gmail.com>
2026-07-27 05:11:25 -0400

    Complete capstone implementation

--- server/database/app.js
+++ server/database/app.js
@@ -11,7 +11,7 @@ app.use(require('body-parser').urlencoded({ extended: false }));
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
 const dealerships_data = JSON.parse(fs.readFileSync("dealerships.json", 'utf8'));
 
-mongoose.connect("mongodb://mongo_db:27017/",{'dbName':'dealershipsDB'});
+mongoose.connect('mongodb://mongo_db:27017/dealershipsDB');
 
 const Reviews = require('./review');
 const Dealerships = require('./dealership');
@@ -46,7 +46,7 @@ app.get('/fetchReviews', async (req, res) => {
 // Express route to fetch reviews by a particular dealer
 app.get('/fetchReviews/dealer/:id', async (req, res) => {
   try {
-    const documents = await Reviews.find({dealership: req.params.id});
+    const documents = await Reviews.find({ dealership: parseInt(req.params.id) });
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
@@ -76,7 +76,7 @@ app.get('/fetchDealers/:state', async (req, res) => {
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
   try {
-    const documents = await Dealerships.find({id: req.params.id});
+    const documents = await Dealerships.find({ id: parseInt(req.params.id) });
     res.json(documents);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching documents' });
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,15 @@
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from './components/Dealers/Dealer';
+import PostReview from "./components/Dealers/PostReview";
+
+// Inside your <Routes> block:
+
+// Inside your Routes:
+
+// Inside your Router / Routes component list:
 
 // Inside your Routes block:
 
@@ -9,6 +18,9 @@ function App() {
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
